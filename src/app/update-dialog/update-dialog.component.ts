a8376c27f5480c96ad18abab94330c0b5782f678
austinmscholl <asprof34@gmail.com>
2018-11-06 17:07:34 -0500

    dk

--- src/app/update-dialog/update-dialog.component.ts
+++ src/app/update-dialog/update-dialog.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { ItemService } from '../item.service';
+import { FormGroup, FormBuilder, FormControl } from '@angular/forms'
 
 @Component({
   selector: 'app-update-dialog',
@@ -9,10 +10,15 @@ import { ItemService } from '../item.service';
 })
 export class UpdateDialogComponent implements OnInit {
 
+  uItemForm: FormGroup
+  uItem: any = []
+
   constructor(
-    public dialogRef: MatDialogRef<UpdateDialogComponent>, private itemService:
-    ItemService
-  ) { }
+    public dialogRef: MatDialogRef<UpdateDialogComponent>, private itemService: ItemService, 
+    @Inject(MAT_DIALOG_DATA) data,
+    private fb: FormBuilder
+  ) { 
+  }
 
   onNoClick(): void {
     this.dialogRef.close()
@@ -24,7 +30,19 @@ export class UpdateDialogComponent implements OnInit {
       .subscribe()
   }
 
+  getUpdateItem(){
+    let id = sessionStorage.getItem('itemId')
+    this.itemService.getUpdateItem(id)
+      .subscribe(items => console.log(items))
+  }
+
   ngOnInit() {
+    this.uItemForm = this.fb.group({
+      itemName: new FormControl(),
+      itemPrice: new FormControl(),
+      itemDescription: new FormControl()
+    })
+    this.getUpdateItem()
   }
 
 }
