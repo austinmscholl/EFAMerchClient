0f6fb1f9a92ad6bceeb5554bbef5fa9d2bf1db38
Drew Blincoe <Drew@drews-mbp.attlocal.net>
2018-11-15 07:59:43 -0500

    styling changes

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 import { AuthService } from '../auth.service';
+import { MIN_LENGTH_VALIDATOR } from '@angular/forms/src/directives/validators';
 
 @Component({
   selector: 'app-auth',
@@ -12,15 +13,17 @@ export class AuthComponent implements OnInit {
   user: FormGroup
   newUser: any = []
   toggleForm = false;
+  submitted: Boolean
 
   constructor(private auth: AuthService, private fb: FormBuilder) { }
 
   ngOnInit() {
     this.user = this.fb.group({
-      email: new FormControl(),
-      password: new FormControl(),
-      firstname: new FormControl(),
-      lastname: new FormControl()
+      email: [''],
+      password:['', [Validators.minLength(6), Validators.maxLength(30), Validators.required]],
+      confirmPass: ['', [Validators.minLength(6), Validators.maxLength(30), Validators.required]],
+      firstname: [''],
+      lastname: ['']
     })
   }
 
@@ -46,12 +49,21 @@ export class AuthComponent implements OnInit {
   }
 
   signUp(){
-    console.log(this.user.controls)
-    this.auth.signUp(this.user.controls.email.value,
-      this.user.controls.password.value,
-      this.user.controls.firstname.value,
-      this.user.controls.lastname.value)
-      .subscribe(user => this.setToken(user))
+    this.submitted = true
+
+    if(!this.user.valid){
+      return
+    }
+
+    if(this.user.value.password === this.user.value.confirmPass){
+      this.auth.signUp(this.user.controls.email.value,
+        this.user.controls.password.value,
+        this.user.controls.firstname.value,
+        this.user.controls.lastname.value)
+        .subscribe(user => this.setToken(user))
+    } else {
+      alert('Passwords need to match!')
+    }
   }
 
 }
